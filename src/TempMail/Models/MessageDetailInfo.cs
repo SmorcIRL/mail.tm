48d77e51483e935baef227801ddbf0314ab0d90e
Seji <seji@tihoda.de>
2023-10-26 21:35:55 +0200

    Add new Model to get all Properties when getting a specific mail by Id

---
+++ src/TempMail/Models/MessageDetailInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace SmorcIRL.TempMail.Models
+{
+    public class MessageDetailInfo : MessageInfo
+    {
+       
+        [JsonProperty("cc")]
+        public string CC { get; set; }
+        
+        [JsonProperty("bcc")]
+        public string BCC { get; set; }
+        
+        [JsonProperty("flagged")]
+        public bool Flagged { get; set; }
+        
+        [JsonProperty("verifications")]
+        public string[] Verifications { get; set; }
+        
+        [JsonProperty("retention")]
+        public bool Retention { get; set; }
+        
+        [JsonProperty("retentionDate")]
+        public DateTime RetentionDate { get; set; }
+        
+        [JsonProperty("text")]
+        public string BodyText { get; set; }
+        
+        [JsonProperty("html")]
+        public string[] BodyHtml { get; set; }
+        
+        [JsonProperty("attachments")]
+        public AttachmentInfo[] Attachments { get; set; }
+    }
+}
