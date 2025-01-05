e440b969007f75858066095b2e51b9de1778fca4
fazelukario <fazelukario@gmail.com>
2025-01-05 07:41:12 +0200

    Add missing models

--- src/TempMail/Models/MessageDetailInfo.cs
+++ src/TempMail/Models/MessageDetailInfo.cs
@@ -1,24 +1,21 @@
 ﻿using System;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace SmorcIRL.TempMail.Models
 {
     public class MessageDetailInfo : MessageInfo
     {
         [JsonProperty("cc")]
-        public string[] CC { get; set; }
+        public UserInfo[] CC { get; set; }
         
         [JsonProperty("bcc")]
-        public string[] BCC { get; set; }
+        public UserInfo[] BCC { get; set; }
         
         [JsonProperty("flagged")]
         public bool Flagged { get; set; }
         
-        // It should be string[] according to the docs, but for me, it was a single object
-        // JToken should allow any kind of JSON
         [JsonProperty("verifications")]
-        public JToken Verifications { get; set; }
+        public VerificationsInfo Verifications { get; set; }
         
         [JsonProperty("retention")]
         public bool Retention { get; set; }
---
+++ src/TempMail/Models/VerificationsInfo.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace SmorcIRL.TempMail.Models
+{
+    public class VerificationsInfo
+    {
+        [JsonProperty("tls")]
+        public TlsInfo Tls { get; set; }
+
+        [JsonProperty("spf")]
+        public bool Spf { get; set; }
+
+        [JsonProperty("dkim")]
+        public bool Dkim { get; set; }
+    }
+}
